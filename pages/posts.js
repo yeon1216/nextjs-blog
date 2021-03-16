dc052048f1cd0a197a071fd2600db43225207fbd
yeon1216 <sykim8770@gmail.com>
2021-03-16 10:11:59 +0900

    init

---
+++ pages/posts.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+export default function FirstPost() {
+    return (
+        <>
+        <h1>
+            First Post
+        </h1>
+        <h2>
+
+            <Link href="/">
+                <a>Back to home</a>
+            </Link>
+
+            {/*<a href={"/"}>back</a>*/}
+        </h2>
+        </>
+    )
+}
